199225a9b6655ae5ac620538601a7a51a288cd91
Derek Westjohn <derek@subwaydev.com>
2021-12-11 11:48:25 -0500

    added logicStep2.js

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -124,27 +124,52 @@ let airportData = "https://raw.githubusercontent.com/dwest85/Mapping_Earthquakes
 let torontoData = "https://raw.githubusercontent.com/dwest85/Mapping_Earthquakes/JSON_URL/JSON_URL/torontoRoutes.json";
 
 
-let myStyle = {
-  color: "black",
-  fillcolor: "#ffa500",
-  weight: 2
-}
+// let myStyle = {
+//   color: "black",
+//   fillcolor: "#ffa500",
+//   weight: 2
+// }
 
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
-    style: myStyle,
-    pointToLayer: function (feature, latlng) {
-      return L.circleMarker(latlng)
 
+    // We turn each feature into a circleMarker on the map.
+    
+    pointToLayer: function(magnitude, latlng) {
+                console.log(data);
+                return L.circleMarker(latlng);
+            },
+          // We set the style for each circleMarker using our styleInfo function.
+        style: styleInfo
+        }).addTo(map);
+    });
 
+      // This function returns the style data for each of the earthquakes we plot on
+  // the map. We pass the magnitude of the earthquake into a function
+  // to calculate the radius.
+  function styleInfo(magnitude) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: "#ffae42",
+      color: "#000000",
+      radius: getRadius(magnitude),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+    // This function determines the radius of the earthquake marker based on its magnitude.
+  // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
     }
-
-
-  }).addTo(map);
-});
+    return magnitude * 4;
+  };
 
 
 
